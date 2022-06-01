2e5f2498be1cb1f620ae7eed5b82e1eb1ffac3d3
Ewa Rusinowska <ewarusinowska12@gmail.com>
2022-06-01 15:11:47 +0200

    built a basic api

---
+++ language_chatbot/api.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+from language_chatbot.gpt_model import chat
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+@app.get("/")
+def index():
+    return {"greeting": "Hello world"}
+
+
+@app.get("/reply")
+
+def reply(text):
+
+    return chat(text)
