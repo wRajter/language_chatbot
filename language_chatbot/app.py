4ca3a48e5d9943ff9fb8a50d301e0be8f7dff810
Nicolai Lewis <nicolai.lewis@gmail.com>
2022-06-02 15:28:41 +0200

    Done the streamlit code

--- language_chatbot/app.py
+++ language_chatbot/app.py
@@ -3,7 +3,7 @@ from streamlit_chat import message as st_message
 
 import requests
 
-url = "http://127.0.0.1:8000/reply"
+url = "https://chatbot-ni4mcaftla-ew.a.run.app/reply"
 
 st.set_page_config(page_title="Multilingual Chatbot", page_icon=":computer:", layout="wide")
 
@@ -15,13 +15,14 @@ with st.container():
     st.subheader("I am here to help you learn a language of your choice :speech_balloon:")
     st.write("---")
 
-def generate_answer(url = "http://127.0.0.1:8000/reply"):
+def generate_answer(url = "https://chatbot-ni4mcaftla-ew.a.run.app/reply"):
     user_message = st.session_state.input_text
-    response = requests.get(url, params=user_message)
+    params = {'text': user_message}
+    response = requests.get(url, params=params)
     answer = response.json()
 
     st.session_state.history.append({"message": user_message, "is_user": True})
-    st.session_state.history.append({"message": answer, "is_user": False})
+    st.session_state.history.append({"message": answer['response'], "is_user": False})
 
 st.text_input("Talk to the bot", key="input_text", on_change=generate_answer)
 
